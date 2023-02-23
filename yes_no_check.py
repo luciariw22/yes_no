a1331979d4f440bbfc14013e823177a34821fe91
luciariw22 <125606190+luciariw22@users.noreply.github.com>
2023-02-24 09:30:18 +1300

    Add files via upload

---
+++ yes_no_check.py
@@ -0,0 +1,9 @@
+# Ask users for a number
+get_number = int(input("Choose a number"))
+
+# Multiply the number by 5
+times_five = get_number * 5
+
+# Output the result
+print("{} times five is equal to "
+      "{}".format(get_number, times_five))
